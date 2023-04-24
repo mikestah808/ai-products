b0d3bec696ca0d65dd0cff1e4a5728df4cd07cc4
Michael Stafford <michaelstafford808@gmail.com>
2023-04-24 18:29:49 -0400

    Created BackDrop, CameraRig, and Shirt function components in canvas folder

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+import Shirt from './Shirt'
+import BackDrop from './BackDrop'
+import CameraRig from './CameraRig'
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+
+      {/* <CameraRig>
+      <BackDrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
