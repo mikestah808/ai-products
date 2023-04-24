5b2e18d5a59d035b298a0ef88aa6ad2988eeefe4
Michael Stafford <michaelstafford808@gmail.com>
2023-04-24 12:42:42 -0400

    installed threejs, valtio, tailwindcss, and added proper files in both public and src folders

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
