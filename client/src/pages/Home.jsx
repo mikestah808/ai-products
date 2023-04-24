50b6ce64008e35e3f4069472271875245a899b61
Michael Stafford <michaelstafford808@gmail.com>
2023-04-24 13:04:14 -0400

    added AnimatePresence, motion section, motion header, and motion div components to Home function component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,44 @@
 import { motion, AnimatePresence } from 'framer-motion';
-import { useSnapshot } from 'valtio';
+import { snapshot, useSnapshot } from 'valtio';
 
 import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion';
 
+import state from '../store'
+
 const Home = () => {
+    const snap = useSnapshot(state)
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+        {snap.intro && (
+            <motion.section className='home' {...slideAnimation('left')}>
+                <motion.header {...slideAnimation('down')}>
+                    <img 
+                    src='./threejs.png'
+                    alt='logo'
+                    className='w-8 h-8 object-contain'
+                    />
+                </motion.header>
+                <motion.div className='home-content' {...headContainerAnimation}>
+                    <motion.div {...headTextAnimation}>
+                        <h1 className='head-text'>
+                            LET'S <br className='xl:block hidden' /> DO IT
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        {...headContentAnimation}
+                        className='flex flex-col gap-5'
+                    >
+                        <p className='max-w-md font-normal text-gray-600 text-base'>
+                        Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your
+                        imagination</strong>{" "}
+                        and define your own style.
+                        </p>
+                    </motion.div>
+                </motion.div>
+            </motion.section>
+        )}
+    </AnimatePresence>
   )
 }
 
