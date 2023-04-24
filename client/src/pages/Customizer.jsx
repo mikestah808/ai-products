28ceb14dc7a143c248fec093137545f4446d39cb
Michael Stafford <michaelstafford808@gmail.com>
2023-04-24 17:24:07 -0400

    created AIPicker, ColorPicker, FilePicker, and Tab files in components folder

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { AnimatePresence , motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import config from '../config/config';
+import store from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, FilePicker, CustomButton, Tab } from "../components";
+
 
 const Customizer = () => {
   return (
