f98d820545d99d8b520df0668c9f246f0f070f3d
Michael Stafford <michaelstafford808@gmail.com>
2023-04-24 21:02:41 -0400

    added conditional for targetPosition

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,12 +10,12 @@ const CanvasModel = () => {
       <ambientLight intensity={0.5}/>
       <Environment preset='city'/>
 
-      {/* <CameraRig>
-      <BackDrop /> */}
+      <CameraRig>
+      {/* <BackDrop /> */}
       <Center>
         <Shirt />
       </Center>
-      {/* </CameraRig> */}
+      </CameraRig>
     </Canvas>
   )
 }
