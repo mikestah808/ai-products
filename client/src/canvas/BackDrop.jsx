5c9e13d2bd013addd853e6b4fc36c70806a620f4
Michael Stafford <michaelstafford808@gmail.com>
2023-04-25 10:22:23 -0400

    imported useRef, easing, useFrame, AccumulativeShadows, and RandomizedLight to BackDrop function component

--- client/src/canvas/BackDrop.jsx
+++ client/src/canvas/BackDrop.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
+
 
 const BackDrop = () => {
   return (
-    <div>BackDrop</div>
+    <AccumulativeShadows
+    position={[0, 0, -0.14]}
+    >
+      <RandomizedLight 
+        amount={4}
+      />
+      
+    </AccumulativeShadows>
   )
 }
 
