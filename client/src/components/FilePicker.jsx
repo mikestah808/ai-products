11085fb39aa36542faf3d75aa27fff93631a320f
Michael Stafford <michaelstafford808@gmail.com>
2023-04-25 13:24:29 -0400

    created handleActiveFilterTab and readFile function in Customizer function component

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const FilePicker = () => {
+import CustomButton from './CustomButton'
+
+const FilePicker = ({ file, setFile, readFile }) => {
   return (
     <div>FilePicker</div>
   )
